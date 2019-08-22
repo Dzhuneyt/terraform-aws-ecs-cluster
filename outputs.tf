b08f175aeff36f201e588a1e64b4d207e7a432bf
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-08-22 15:37:02 +0300

    feat: New output to provide to consumers of the module the SG for EC2 instances

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,6 @@ output "vpc_id" {
 output "service_discovery_id" {
   value = aws_service_discovery_private_dns_namespace.dns_namespace.id
 }
+output "security_group_for_ec2_instances" {
+  value = aws_security_group.sg_for_ec2_instances.id
+}
