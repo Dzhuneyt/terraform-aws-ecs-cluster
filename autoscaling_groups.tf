bfacfa9218abb8e886a037c2f6528c2385777b09
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-08-24 11:39:37 +0300

    fix: ASG creation could easily fail if the spot price was set too low, causing the deployment to hang

--- autoscaling_groups.tf
+++ autoscaling_groups.tf
@@ -2,13 +2,13 @@
 resource "aws_autoscaling_group" "ecs_cluster_ondemand" {
   name_prefix = "${var.cluster_name}_asg_ondemand_"
   termination_policies = [
-  "OldestInstance"]
-  default_cooldown          = 30
+    "OldestInstance"]
+  default_cooldown = 30
   health_check_grace_period = 30
-  max_size                  = var.max_ondmand_instances
-  min_size                  = var.min_ondemand_instances
-  desired_capacity          = var.min_ondemand_instances
-  launch_configuration      = aws_launch_configuration.ecs_config_launch_config_ondemand.name
+  max_size = var.max_ondmand_instances
+  min_size = var.min_ondemand_instances
+  desired_capacity = var.min_ondemand_instances
+  launch_configuration = aws_launch_configuration.ecs_config_launch_config_ondemand.name
   lifecycle {
     create_before_destroy = true
   }
@@ -16,8 +16,8 @@ resource "aws_autoscaling_group" "ecs_cluster_ondemand" {
 
   tags = [
     {
-      key                 = "Name"
-      value               = var.cluster_name,
+      key = "Name"
+      value = var.cluster_name,
       propagate_at_launch = true
     }
   ]
@@ -26,13 +26,18 @@ resource "aws_autoscaling_group" "ecs_cluster_ondemand" {
 resource "aws_autoscaling_group" "ecs_cluster_spot" {
   name_prefix = "${var.cluster_name}_asg_spot_"
   termination_policies = [
-  "OldestInstance"]
-  default_cooldown          = 30
+    "OldestInstance"]
+  default_cooldown = 30
   health_check_grace_period = 30
-  max_size                  = var.max_spot_instances
-  min_size                  = var.min_spot_instances
-  desired_capacity          = var.min_spot_instances
-  launch_configuration      = aws_launch_configuration.ecs_config_launch_config_spot.name
+  max_size = var.max_spot_instances
+  min_size = var.min_spot_instances
+  desired_capacity = var.min_spot_instances
+  launch_configuration = aws_launch_configuration.ecs_config_launch_config_spot.name
+
+  # Allow the ASG to be created through Terraform,
+  # even if the price is too low or there are other
+  # issues with placing the spot request
+  wait_for_elb_capacity = "0"
   lifecycle {
     create_before_destroy = true
   }
@@ -40,8 +45,8 @@ resource "aws_autoscaling_group" "ecs_cluster_spot" {
 
   tags = [
     {
-      key                 = "Name"
-      value               = var.cluster_name,
+      key = "Name"
+      value = var.cluster_name,
       propagate_at_launch = true
     }
   ]
@@ -49,8 +54,8 @@ resource "aws_autoscaling_group" "ecs_cluster_spot" {
 
 # Attach an autoscaling policy to the spot cluster to target 70% MemoryReservation on the ECS cluster.
 resource "aws_autoscaling_policy" "ecs_cluster_scale_policy" {
-  name            = "${var.cluster_name}_ecs_cluster_spot_scale_policy"
-  policy_type     = "TargetTrackingScaling"
+  name = "${var.cluster_name}_ecs_cluster_spot_scale_policy"
+  policy_type = "TargetTrackingScaling"
   adjustment_type = "ChangeInCapacity"
   lifecycle {
     ignore_changes = [
@@ -62,12 +67,12 @@ resource "aws_autoscaling_policy" "ecs_cluster_scale_policy" {
   target_tracking_configuration {
     customized_metric_specification {
       metric_dimension {
-        name  = "ClusterName"
+        name = "ClusterName"
         value = var.cluster_name
       }
       metric_name = "MemoryReservation"
-      namespace   = "AWS/ECS"
-      statistic   = "Average"
+      namespace = "AWS/ECS"
+      statistic = "Average"
     }
     target_value = 70.0
   }
